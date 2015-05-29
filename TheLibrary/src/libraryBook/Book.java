94161a9f0299dcf93205417744e8437b653fe092
MattCardoso <mat.cardoso25@gmail.com>
2015-05-28 23:14:16 -0300

    Method toString added for Book class

--- TheLibrary/src/libraryBook/Book.java
+++ TheLibrary/src/libraryBook/Book.java
@@ -1,9 +1,29 @@
 package libraryBook;
 
 public class Book {
-	private int ID; // ID para a indentificacao do livro
-	private String Title; // Titulo do livro	
-	private int type;	// Tipo do Livro: 1- Texto; 2- Geral;
-	private String Content; // Variavel adicional para o conteudo do livro(???)
+	private int 	 _id; // ID para a indentificacao do livro
+	private String	 _title; // Titulo do livro	
+	private int		 _type;	// Tipo do Livro: 1- Texto; 2- Geral;
+	private String	 _content; // Variavel adicional para o conteudo do livro(???)
 	
+	public Book(int ID, String Title, int Type, String Content, int Quantity)
+	{
+		_id 		= ID;
+		_title 		= Title;
+		_type 		= Type;
+		_content	= Content;
+	}
+	
+	
+	
+	public String toString(){
+		return "" + this._id + "|" + this._title + "|" + this._type + "|" +this._content;
+	}
+	
+	//Getters and Setter
+	public int getID(){return this._id;}
+	public void setID(int id){ this._id = id;}
+	
+	public int getType(){return _type;}
+	public void setType(int type){this._type = type;}
 }
