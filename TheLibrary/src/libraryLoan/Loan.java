5dfab68403fd8b0a6630b712d12df0496a1c07d4
MattCardoso <mat.cardoso25@gmail.com>
2015-05-28 23:13:33 -0300

    Method toString added for Loan class

--- TheLibrary/src/libraryLoan/Loan.java
+++ TheLibrary/src/libraryLoan/Loan.java
@@ -4,7 +4,24 @@ import libraryBook.Book;
 import libraryUsers.User;
 
 public class Loan {
-	private Book book;
-	private User user;
+	private Book _book;
+	private User _user;
 	private int _lentDate, _deliveryDate;
+	
+	public Loan(Book book, User user, int currentTime){
+		this._book = book;
+		this._user = user;
+		// verificar se User prof ou aluno ou comunidade e depois
+		// settar as datas aqui com base no currentTime
+
+	}
+	
+	public String toString(){
+		return _book.getID() + "|" + _user.getID();
+	}
+	
+	//Getters and Setters
+	public User getUser(){return this._user;}
+	
+	public Book getBook(){return this._book;}
 }
