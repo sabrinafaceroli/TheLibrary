92b0b05eb2bc78aa6f950c03b6c91fc41338e0ad
MattCardoso <mat.cardoso25@gmail.com>
2015-05-28 23:00:08 -0300

    Method toString Added for each User type Class

--- TheLibrary/src/libraryUsers/Community.java
+++ TheLibrary/src/libraryUsers/Community.java
@@ -7,11 +7,17 @@ import libraryLoan.Loan;
 public class Community extends User{
 	private String _job;
 	
-	
-	
 	Community(int id, String name, String doc, String email, String password) {
 		super(id, name, doc, email, password);
 		this.setLoans(new ArrayList<Loan>(2));
 	}
-
+	Community(int id, String name, String doc, String email, String password, String job) {
+		super(id, name, doc, email, password);
+		this._job = job;
+		this.setLoans(new ArrayList<Loan>(2));
+	}
+	
+	public String toString(){
+		return super.toString() + " " + this._job;
+	}
 }
