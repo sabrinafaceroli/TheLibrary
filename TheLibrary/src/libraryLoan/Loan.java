ebd0cd692563ef46cef0dc372d523f64120d35de
MattCardoso <mat.cardoso25@gmail.com>
2015-06-01 08:10:13 -0300

    Added date field and its functions

--- TheLibrary/src/libraryLoan/Loan.java
+++ TheLibrary/src/libraryLoan/Loan.java
@@ -1,25 +1,48 @@
 package libraryLoan;
 
+import java.util.GregorianCalendar;
+
 
 public class Loan {
 	private int _bookID;
 	private int _userID;
-	private int _lentDate, _deliveryDate;
+	private GregorianCalendar _lentDate, _deliveryDate;
+	private boolean _isLate;
 	
-	public Loan(int book, int user, int currentTime){
+	public Loan(int book, int user, String lentDate, String deliveryDate, String SystemTime){
+		String[] date = lentDate.split("/");
+		
 		this._bookID = book;
 		this._userID = user;
-		// verificar se User prof ou aluno ou comunidade e depois
-		// settar as datas aqui com base no currentTime
-
+		this._lentDate = new GregorianCalendar(Integer.parseInt(date[2]), Integer.parseInt(date[1]), Integer.parseInt(date[0]));
+		date = deliveryDate.split("/");
+		this._deliveryDate = new GregorianCalendar(Integer.parseInt(date[2]), Integer.parseInt(date[1]), Integer.parseInt(date[0]));
+		this._isLate = this.checkTardiness(SystemTime);
 	}
 	
-	public Loan(String csv){
+	public Loan(String csv, String SystemTime){
+		String[] date;
         String[] values = csv.split(",");
         this._bookID = Integer.parseInt(values[0]);
         this._userID = Integer.parseInt(values[1]);
-        //Adicionar os time stamps
+        date = values[2].split("/");
+        this._lentDate = new GregorianCalendar(Integer.parseInt(date[2]), Integer.parseInt(date[1]), Integer.parseInt(date[0]));
+        date = values[3].split("/");
+        this._deliveryDate = new GregorianCalendar(Integer.parseInt(date[2]), Integer.parseInt(date[1]), Integer.parseInt(date[0]));
+        this._isLate = this.checkTardiness(SystemTime);
+    }
+	
+	private boolean checkTardiness(String SystemTime){
+		String[] date = SystemTime.split("/");
+		GregorianCalendar comparator = new GregorianCalendar(Integer.parseInt(date[2]), Integer.parseInt(date[1]), Integer.parseInt(date[0]));
+		
+		if(comparator.after(comparator))
+			return true;
+		
+		return false;
 	}
+	
+	public boolean isLate(){return this._isLate;}
 	
 	//Getters and Setters
 	public int getUser(){return this._userID;}
@@ -27,6 +50,7 @@ public class Loan {
 	public int getBook(){return this._bookID;}
 	
 	public String toString(){
-		return this._bookID + "," + this._userID; //Tem que ter as datas aqui 
+		return this._bookID + "," + this._userID + "," + this._lentDate + "," 
+				+ this._deliveryDate + "," +this._isLate; 
 	}
 }
