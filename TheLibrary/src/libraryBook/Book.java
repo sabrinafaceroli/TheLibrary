d92efab027e9c92969fb820ed65eb6368fd393fd
MattCardoso <mat.cardoso25@gmail.com>
2015-06-01 08:09:20 -0300

    Added some fields(authorr, publisher, edition and pages)

--- TheLibrary/src/libraryBook/Book.java
+++ TheLibrary/src/libraryBook/Book.java
@@ -2,16 +2,23 @@ package libraryBook;
 
 public class Book {
 	private int 	 _id; // ID para a indentificacao do livro
-	private String	 _title; // Titulo do livro	
 	private int		 _type;	// Tipo do Livro: 1- Texto; 2- Geral;
-	private String	 _content; // Variavel adicional para o conteudo do livro(???)
+	private String	 _title; // Titulo do livro	
+	private String	 _author;
+	private String	 _publisher;
+	private int		 _edition;
+	private int 	 _pages;
 	
-	public Book(int ID, String Title, int Type, String Content) //int Quantity)
+	public Book(int ID, String Title, int Type, String Publisher, String Author, int Edition, int Pages) //int Quantity)
 	{
 		this._id 		= ID;
 		this._title 	= Title;
 		this._type 		= Type;
-		this._content	= Content;
+		this._author 	= Author;
+		this._publisher	= Publisher;
+		this._edition 	= Edition;
+		this._pages 	= Pages;
+		
 	}
 	
 	public Book(String csv)
@@ -20,11 +27,15 @@ public class Book {
         this._id = Integer.parseInt(values[0]);
         this._title = values[1];
         this._type = Integer.parseInt(values[2]);
-        this._content= values[3];
+        this._author = values[3];
+        this._publisher = values[4];
+        this._edition = Integer.parseInt(values[5]);
+        this._pages = Integer.parseInt(values[6]);
 	}
 
 	public String toString(){
-		return "" + this._id + "," + this._title + "," + this._type + "," +this._content;
+		return "" + this._id + "," + this._title + "," + this._type + "," +this._author +
+				"," + this._publisher + "," + this._edition + "," + this._pages;
 	} 
 	
 	//Getters and Setter
@@ -37,6 +48,6 @@ public class Book {
 	public String getTitle(){return this._title;}
 	public void setTitle(String title){this._title = title;}
 	
-	public String getContent(){return this._content;}
-	public void setContent(String content){this._content = content;}
+	public String getAuthor(){return this._author;}
+	public void setAuthor(String author){this._author = author;}
 }
